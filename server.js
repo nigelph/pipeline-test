046727cbed98118bfb13ba43093e193fe05dbb2a
Nigel Phan <nigel.phan11@gmail.com>
2022-02-28 23:35:28 +1300

    added gitignore, jest, unit test

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const app = express();
-app.use(express.json())
+// const express = require('express');
+// const app = express();
+// app.use(express.json())
 
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
 
-})
+// })
 
 
 
-app.listen(4000, () => console.log('Server is running!'))
+// app.listen(4000, () => console.log('Server is running!'))
+
+
+function sum(a, b) {
+    return a + b;
+}
---
+++ sum.spec.js
@@ -0,0 +1,12 @@
+
+const sum = require('./sum')
+
+describe('adding numbers together', function () {
+    it('1 + 2 should be equal to 3', () => {
+        // const input =
+        // const expected =
+        // const actual =
+        // expect(actual).toBe(expected)
+        expect(sum(1, 2)).toBe(3)
+    })
+})
