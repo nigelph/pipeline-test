b258a3ea4ebe3776b33c12eec42ab36b4c75d8f7
Nigel Phan <nigel.phan11@gmail.com>
2022-03-01 12:33:19 +1300

    added another test case

--- sum.spec.js
+++ sum.spec.js
@@ -3,10 +3,17 @@ const sum = require('./sum')
 
 describe('adding numbers together', function () {
     it('1 + 2 should be equal to 3', () => {
-        // const input =
-        // const expected =
-        // const actual =
-        // expect(actual).toBe(expected)
-        expect(sum(1, 2)).toBe(3)
+        const inputOne = 1
+        const inputTwo = 2
+        const expected = inputOne + inputTwo
+        const actual = sum(inputOne, inputTwo)
+        expect(actual).toBe(expected)
+    })
+    it('1 + 2 should be equal to 3', () => {
+        const inputOne = 220
+        const inputTwo = 230
+        const expected = inputOne + inputTwo
+        const actual = sum(inputOne, inputTwo)
+        expect(actual).toBe(expected)
     })
 })
